f4d2129009553e27992d6005e3f302a977ac7c7f
wendywong1129 <wendywong.qw@gmail.com>
2023-05-15 17:12:36 +1000

    first commit

---
+++ server/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from Server!",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+    console.log("response.data: ", response.data);
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || "Something went wrong");
+  }
+});
+
+app.listen(8081, () =>
+  console.log("AI server started on http://localhost:8081")
+);
